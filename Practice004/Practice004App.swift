26b7e25b1fb46fb49aead744efd3facabe34a54d
masker_dog <37284851+MaskerDog@users.noreply.github.com>
2023-06-19 10:42:04 +0900

    フォントライセンスの設定

--- Practice004/Practice004App.swift
+++ Practice004/Practice004App.swift
@@ -5,6 +5,11 @@
 //  Created by npc on 2023/06/15.
 //
 
+//このアプリケーションには、SIL OPEN FONT LICENSE Version 1.1で定められた条件に基づいて配布されているフォントが含まれています。
+//フォントの詳細およびライセンステキストについては、以下のリンク先を参照してください:
+//「M+ FONTS」([http://jikasei.me/font/mgenplus/](https://mplusfonts.github.io/))
+//Licensed under SIL Open Font License 1.1 (http://scripts.sil.org/OFL)
+
 import SwiftUI
 
 @main
